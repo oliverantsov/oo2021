48a7aeeb7b5c74d7b2786f715c5838bfb5985fd8
Oliver Antsov <oliver11@tlu.ee>
2021-02-19 15:01:00 +0200

    Week 2 modified Main file.

--- w02_klass/Main.java
+++ w02_klass/Main.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        linkedlist();
+        lists();
     }
 
     public static void characters(){
