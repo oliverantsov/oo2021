6cddd4a37d1ffa56b9ce793ee4746fdaf0065580
Oliver Antsov <oliverantsov@users.noreply.github.com>
2021-04-29 21:55:08 +0300

    Week 9. Threads, streams and try-catching.

---
+++ w09_tunnirakenduse_lahendus/ThreadExample.java
@@ -0,0 +1,27 @@
+public class ThreadExample {
+    public static String[] names = { "Kati", "Mari", "Mati", "Ants", "Juku", "Oliver", "Tõnu", "Juss" };
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++){
+            try {
+                new Thread(getRunnable(), names[i]).start();
+                Thread.sleep(500);
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static Runnable getRunnable() {
+        return () -> {
+            for (int i = 0; i < 8; i++){
+                try {
+                    System.out.println(Thread.currentThread().getName() + " " + i);
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        };
+    }
+}
