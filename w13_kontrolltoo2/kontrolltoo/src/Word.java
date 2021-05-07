5e38fb2bdb08ac4ed232273e4883cc46a73c7f25
Oliver Antsov <oliverantsov@users.noreply.github.com>
2021-05-07 13:50:54 +0300

    Week 13. Test.

--- w13_kontrolltoo2/kontrolltoo/src/Word.java
+++ w13_kontrolltoo2/kontrolltoo/src/Word.java
@@ -1,16 +1,26 @@
 import java.util.Scanner;
 
 public class Word implements WordInterface {
-    Scanner scanner = new Scanner(System.in);
 
     @Override
     public int getLetterCount() {
+        Scanner scanner = new Scanner(System.in);
         int letterCount = 0;
         int i = 0;
-        for(i = 0; i < word.length; i++){
-            letterCount = letterCount + 1;
+
+        System.out.println("Enter random word: ");
+        String userWord = scanner.nextLine();
+        System.out.println("Enter random letter to check from previously entered word: ");
+        char userLetter = scanner.next().charAt(0);
+        
+        for(userLetter = 'a'; userLetter <= 'ü'; userLetter++){
+            for(i = 0; i < userWord.length(); i++){
+
+                letterCount = letterCount + 1;
+            }
         }
+        
         return letterCount;
+
     }
-    
 }
