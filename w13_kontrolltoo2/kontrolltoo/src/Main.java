f7debe1b44fb11c92feef4637696c309999e4c82
Oliver Antsov <oliverantsov@users.noreply.github.com>
2021-05-12 19:02:23 +0300

    Week 13. Test (2p/3p).

--- w13_kontrolltoo2/kontrolltoo/src/Main.java
+++ w13_kontrolltoo2/kontrolltoo/src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        //Word word = new Word();
-        //System.out.println(word.getLetterCount());
-        //System.out.println("--------------------------------------------------");
+        Word word = new Word();
+        System.out.println("-----------------------------------------------------");
+        System.out.println(word.getLetterCount());
+        System.out.println("-----------------------------------------------------");
         Sentence sentence = new Sentence();
         System.out.println(sentence.getLetterCount());
+        System.out.println("-----------------------------------------------------");
+        //System.out.println(sentence.fileStuff());
     }
 }
--- w13_kontrolltoo2/kontrolltoo/src/Sentence.java
+++ w13_kontrolltoo2/kontrolltoo/src/Sentence.java
@@ -1,17 +1,18 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sentence implements WordInterface {
-    int wordCount;
-
 
     @Override
     public int getLetterCount() {
         Scanner scanner = new Scanner(System.in);
         char letterA = 'a';
-        int letterACountInWord = 0;
         int letterACountInSentence = 0;
         int i = 0;
-
+        int x = 0;
+        int y = 0;
+        int wordCount = 0;
+        
         System.out.print("Enter random sentence: ");
         String lowerCaseUserSentence = scanner.nextLine().toLowerCase();
 
@@ -20,25 +21,43 @@ public class Sentence implements WordInterface {
 
         String[] wordsInSentence = lowerCaseUserSentence.split(" ");
 
+
         for(String word : wordsInSentence){
             //System.out.print(word + " ");
-            if(word.charAt(i) == letterA){
-                letterACountInWord = letterACountInWord + 1;
-            }
+            //System.out.println(Arrays.toString(wordsInSentence));
+            wordCount = wordCount + 1;
         }
 
+        System.out.println("------------------------------");
+        System.out.println("WORDS in entered sentence: " + wordCount);
+        System.out.println("------------------------------");
+
+        for(x = 0; x < wordsInSentence.length; x++){
+            int letterACountInWord = 0;
+            String singleWord = wordsInSentence[x];
+            for(y = 0; y < singleWord.length(); y++){
+                if(singleWord.charAt(y) == letterA){
+                    letterACountInWord = letterACountInWord + 1;
+                }
+            }
+            System.out.println("WORD " + (x + 1) + " 'a' count: " + letterACountInWord);
+        }
+
+// -----------------------------------------------------------------------
+//                                  DONE
         for(i = 0; i < lowerCaseUserSentence.length(); i++){
             if(lowerCaseUserSentence.charAt(i) == letterA){
                 letterACountInSentence = letterACountInSentence + 1;
             }
         }
 
-        
-        System.out.println();
-        System.out.print("Your randomly entered sentence contains following amount of 'a' letters: ");
+        System.out.println("-----------------------------------------------------");
+        System.out.print("SENTENCE contains following amount of 'a' letters: ");
         return letterACountInSentence;
-
-
+// -----------------------------------------------------------------------
     }
 
+    public String fileStuff(){
+        return "Test";
+    }
 }
